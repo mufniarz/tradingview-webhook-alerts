8fc215ec8c419885a38c5d52fbbc4f405bb5af45
mufniarz <marcinmu330@gmail.com>
2022-05-03 16:20:08 -0500

    updated logic

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ BASE_URL = "https://paper-api.alpaca.markets"
 ORDERS_URL = "{}/v2/orders".format(BASE_URL)
 HEADERS = {'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY}
 
+qty = "1000"
+
 @app.route('/buy_stock', methods=['POST'])
 def buy_stock():
     request = app.current_request
@@ -16,18 +18,49 @@ def buy_stock():
 
     data = {
         "symbol": webhook_message['ticker'],
-        "qty": 1,
+        "qty": qty,
         "side": "buy",
-        "type": "limit",
-        "limit_price": webhook_message['close'],
-        "time_in_force": "gtc",
-        "order_class": "bracket",
-        "take_profit": {
-            "limit_price": webhook_message['close'] * 1.05
-        },
-        "stop_loss": {
-            "stop_price": webhook_message['close'] * 0.98,
-        }
+        "type": "market",
+        # "limit_price": webhook_message['close'],
+        # "time_in_force": "gtc",
+        # "order_class": "bracket",
+        # "take_profit": {
+        #     "limit_price": webhook_message['close'] * 1.05
+        # },
+        # "stop_loss": {
+        #     "stop_price": webhook_message['close'] * 0.98,
+        # }
+    }
+
+    r = requests.post(ORDERS_URL, json=data, headers=HEADERS)
+
+    response = json.loads(r.content) 
+
+    return {
+        'webhook_message': webhook_message,
+        'id': response['id'],
+        'client_order_id': response['client_order_id']
+    }
+
+@app.route('/sell_stock', methods=['POST'])
+def buy_stock():
+    request = app.current_request
+    webhook_message = request.json_body
+
+    data = {
+        "symbol": webhook_message['ticker'],
+        "qty": qty,
+        "side": "sell",
+        "type": "market",
+        # "limit_price": webhook_message['close'],
+        # "time_in_force": "gtc",
+        # "order_class": "bracket",
+        # "take_profit": {
+        #     "limit_price": webhook_message['close'] * 1.05
+        # },
+        # "stop_loss": {
+        #     "stop_price": webhook_message['close'] * 0.98,
+        # }
     }
 
     r = requests.post(ORDERS_URL, json=data, headers=HEADERS)
